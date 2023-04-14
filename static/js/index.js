a8672fb7edb71ce83bded93892742c9ac78663fd
Ijdodo <130724080+Ijdodo@users.noreply.github.com>
2023-04-14 07:23:12 +0200

    Add

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,16 @@
 document.getElementById("form").addEventListener("submit", checkForm);
 
-function checkForm(e) {
-    e.preventDefault();
-    let form = e.target;
-    const firstName = form["firstName"].value;
+
+function checkForm(e) {//Fonction pour verifier le formulaire 
+
+    e.preventDefault();// Empêcher la soumission du formulaire par défaut
+
+    let form = e.target;    // Obtenir le formulaire qui a été soumis
+    const firstName = form["firstName"].value;    // Obtenir les valeurs des champs de formulaire
     const lastName = form["lastName"].value;
     const email = form["email"].value;
     const message = form["message"].value;
-    if (firstName.length < 2 || firstName.length > 50) {
+    if (firstName.length < 2 || firstName.length > 50) {    // Vérifier si les valeurs des champs de formulaire sont valides
         displayErrorMessage(firstName, "prénom")
     } else if (lastName.length < 2 || lastName.length > 50) {
         displayErrorMessage(lastName, "nom")
@@ -16,26 +19,25 @@ function checkForm(e) {
     } else if (message.length < 20 || email.length > 500) {
         displayErrorMessage(message, "message")
     } else {
-        document.getElementById("success-message").style.display = "block";
-        localStorage.setItem("formSubmitted", "true");    }
-    
+        document.getElementById("success-message").style.display = "block";        // Si toutes les valeurs du formulaire sont valides, afficher un message de réussite et stocker une variable dans localStorage qui nous permettra de pouvoir garder le message meme si la page refresh
+        localStorage.setItem("formSubmitted", "true");
+    }
+
 }
 
 const form = document.getElementById("contact-form");
 const successMsg = document.getElementById("success-msg");
 
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  if (validateForm()) {
-    successMsg.style.display = "block";
-  }
+    event.preventDefault();
+    if (validateForm()) {
+        successMsg.style.display = "block";
+    }
 });
 
 function validateForm() {
-  // Ajoutez ici votre code de validation de formulaire existant
-  // Assurez-vous de renvoyer true si le formulaire est valide et false sinon.
 }
-
+//Fonction pr afficher message d'erreur si un des champs du formulaire n'est pas rempli correctement 
 function displayErrorMessage(field, name) {
     const errorMessage = document.getElementById("error-message");
     errorMessage.innerHTML = `${field} n'est pas un ${name} valide.`;
@@ -43,22 +45,22 @@ function displayErrorMessage(field, name) {
 }
 
 
-
+// Fonction pour valider une adresse e-mail
 function validateEmail(email) {
     return String(email)
         .toLowerCase()
         .match(
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
+}
+
+window.addEventListener("load", function () {
+    // Vérifier si le formulaire a été soumis avec succès
+    var formSubmitted = localStorage.getItem("formSubmitted");
+        // Si le formulaire a été soumis avec succès
+    if (formSubmitted === "true") {
+        // Afficher le message de réussite
+        var successMessage = document.getElementById("success-message");
+        successMessage.style.display = "block";
     }
-    
-    window.addEventListener("load", function() {
-        // Vérifier si le formulaire a été soumis avec succès
-        var formSubmitted = localStorage.getItem("formSubmitted");
-        if (formSubmitted === "true") {
-          // Afficher le message de réussite
-          var successMessage = document.getElementById("success-message");
-          successMessage.style.display = "block";
-        }
-      });
-      
+});
